d344f7cd0f96ec32b40062fdb5aab9c77ccb019b
Yousef Gilany <yogilany@gmail.com>
2023-02-25 22:47:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./e-logo.svg";
+import picture from "./IMG_0829.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Eventbrite is under construction.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
