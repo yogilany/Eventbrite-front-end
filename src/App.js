62ee37264999c96ceccadec6e57e4a85d2f11c26
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-03-08 19:58:28 +0200

    Likes and Following Pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Layout } from "./app/layout";
 import { Col, Row, Container } from "react-bootstrap";
 import { Header } from "./components/HomePage/Header";
 import { Events } from "./components/HomePage/Events";
-
+import Likes from './Pages/Likes/Likes'
+import Following from './Pages/Following/Following'
 function App() {
   return (
     <Layout>
-      <Header />
-      <Events />
+      {/* <Header />
+      <Events /> */}
+      {/* <Likes /> */}
+      <Following />
     </Layout>
   );
 }
