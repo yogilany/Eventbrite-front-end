d387fd08dae022aebc1bfd5549951685408dbd52
Yousef Gilany <yogilany@gmail.com>
2023-03-08 21:24:29 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Layout } from "./app/layout";
 import { Col, Row, Container } from "react-bootstrap";
 import { Header } from "./components/HomePage/Header";
 import { Events } from "./components/HomePage/Events";
+import { HomePage } from "./components/HomePage/HomePage";
+import { BrowseEventsPage } from "./components/BrowseEventsPage/BrowseEventsPage";
 
 function App() {
   return (
     <Layout>
-      <Header />
-      <Events />
+      <BrowseEventsPage />
     </Layout>
   );
 }
