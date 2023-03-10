6f42ee9d7f5ad4c1942f0da8d1ef09a1524254c4
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-03-11 00:30:25 +0200

    Add Styled Likes Page

---
+++ src/Pages/Likes/Likes.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './likes.css'
+import Like from './Components/LikeBox/Like'
+import Like1 from "../../assets/like1.jpeg";
+import Like2 from "../../assets/like2.jpeg";
+import Like3 from "../../assets/like3.jpeg";
+import Footer from "../../components/footer/Footer"
+const Likes = () => {
+  return (
+    <div className="likes__container">
+      <div className="likes__container-heading">
+        <h1>Likes</h1>
+        <Like
+          Title="E7kky to Empower: Beyond the Story!"
+          Date="Fri, Mar 10, 9:00 AM"
+          Location="Greek Campus, Ad Dawawin"
+          Price="Free"
+          Photo={Like1}
+        ></Like>
+        <Like
+          Title="Arab Public Health Assosciation 3rd International Conference"
+          Date="Sun, Mar 12, 8:00 AM"
+          Location="InterContinental Cairo Semiramis, an IHG Hotel, Qasr Ad Dobarah"
+          Price="Starts at €120.00"
+          Photo={Like2}
+        ></Like>
+        <Like
+          Title="CABLEXX"
+          Date="Wed, Mar 15, 9:00 AM"
+          Location="Dusit Thani Lakeview Cairo, Cairo"
+          Price="Free"
+          Photo={Like3}
+        ></Like>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Likes
