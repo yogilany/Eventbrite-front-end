bf9688b11df3004b039207f40a16679057fdef17
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-03-11 00:34:40 +0200

    Add styled Decription section in Details Page

---
+++ src/Pages/Details/Components/Description/Description.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './description.css'
+import { MdTextFields } from 'react-icons/md'
+import { MdDelete } from 'react-icons/md'
+const Description = () => {
+    const [addText , setText] = useState(false);
+    const [addImage , setImage] = useState(false);
+    const [addVideo , setVideo] = useState(false);
+  return (
+    <div className="description__container">
+      <MdTextFields className = "text__logo" />
+      <h1 style={{ marginBottom: "7px" }}>Description</h1>
+      <p style={{ fontSize: '14px' }}>Add more details to your event like your schedule, sponsors, or featured guests.<a href="#" className='see__examples'> Learn more</a></p>
+      {addText && <div className='add__text'><textarea id = "AddText" /><button onClick = {() => setText(!addText)}><MdDelete /></button></div>}
+      {addImage && <div className='add__image'><textarea id = "AddImage"/><button onClick = {() => setImage(!addImage)}><MdDelete /></button></div>}
+      {addVideo && <div className='add__video'><textarea id = "AddVideo" /><button onClick = {() => setVideo(!addVideo)}><MdDelete /></button></div>}
+      <div className='description__container-buttons'>
+        <button className='cond__btn' onClick = {() => setText(true)}>Add Text</button>
+        <button className='cond__btn' onClick = {() => setImage(true)}>Add Image</button>
+        <button className='cond__btn' onClick = {() => setVideo(true)}>Add Video</button>
+      </div>
+    </div>
+  );
+}
+
+export default Description
