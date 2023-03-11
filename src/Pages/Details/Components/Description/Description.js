013afd6cec5b1cdb0f53312089f3dbde9f31c67d
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-03-11 19:13:37 +0200

    Modify some Styles in Description Section

--- src/Pages/Details/Components/Description/Description.js
+++ src/Pages/Details/Components/Description/Description.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './description.css'
 import { MdTextFields } from 'react-icons/md'
 import { MdDelete } from 'react-icons/md'
+import UploadBox from '../UploadBox/UploadBox'
 const Description = () => {
     const [addText , setText] = useState(false);
     const [addImage , setImage] = useState(false);
@@ -12,7 +13,7 @@ const Description = () => {
       <h1 style={{ marginBottom: "7px" }}>Description</h1>
       <p style={{ fontSize: '14px' }}>Add more details to your event like your schedule, sponsors, or featured guests.<a href="#" className='see__examples'> Learn more</a></p>
       {addText && <div className='add__text'><textarea id = "AddText" /><button onClick = {() => setText(!addText)}><MdDelete /></button></div>}
-      {addImage && <div className='add__image'><textarea id = "AddImage"/><button onClick = {() => setImage(!addImage)}><MdDelete /></button></div>}
+      {addImage && <div className='add__image'><UploadBox></UploadBox> <button onClick = {() => setImage(!addImage)}><MdDelete /></button></div>}
       {addVideo && <div className='add__video'><textarea id = "AddVideo" /><button onClick = {() => setVideo(!addVideo)}><MdDelete /></button></div>}
       <div className='description__container-buttons'>
         <button className='cond__btn' onClick = {() => setText(true)}>Add Text</button>
