104c1e806f6928c174e95bd9caa11f39f35b28e3
Yousef Gilany <yogilany@gmail.com>
2023-03-14 00:26:45 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import "./App.css";
 import { Layout } from "./app/layout";
-import { Col, Row, Container } from "react-bootstrap";
-import BasicInfo from "./Pages/CreatorsView/Basicinfo/BasicInfo";
-import Likes from "./Pages/AttendeesView/Likes/Likes";
-import FollowingOrgEvents from "./Pages/AttendeesView/FollowingOrganizersEvents/Following";
-import SingleEvent from "./Pages/AttendeesView/SingleEvent/SingleEvent";
-import Login from "./Pages/AttendeesView/Login/Login";
-import { HomePage } from "./Pages/AttendeesView/HomePage/HomePage";
-import { AllRoutes } from "./app/Routes";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthenticatedApp from "./app/AuthenticatedApp";
 import UnauthenticatedApp from "./app/UnauthenticatedApp";
 function App() {
