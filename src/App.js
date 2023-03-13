07f7cc94ef0aef854da1f6e57f0b9e716c266e2a
Yousef Gilany <yogilany@gmail.com>
2023-03-14 00:24:34 +0200

    routing to authenticated or unauthenticated apps

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import "./App.css";
 import { Layout } from "./app/layout";
 import { Col, Row, Container } from "react-bootstrap";
-import { Header } from "./components/HomePage/Header";
-import { Events } from "./components/HomePage/Events";
-import { HomePage } from "./components/HomePage/HomePage";
-import { BrowseEventsPage } from "./components/BrowseEventsPage/BrowseEventsPage";
-import { Login } from "./components/Login/Login.js";
-import { Signup } from "./components/Signup/Signup.js";
-import Event from "./components/Events/Event";
-import Details from "./Pages/Details/Details";
-import Likes from "./Pages/Likes/Likes";
-import Following from "./Pages/Following/Following";
-// import Likes from "./Pages/Likes/Likes";
-// import Following from "./Pages/Following/Following";
-import Organizer from "./Pages/Organizer/Organizer";
-import Basicinfo from "./Pages/Basicinfo/Basicinfo";
+import BasicInfo from "./Pages/CreatorsView/Basicinfo/BasicInfo";
+import Likes from "./Pages/AttendeesView/Likes/Likes";
+import FollowingOrgEvents from "./Pages/AttendeesView/FollowingOrganizersEvents/Following";
+import SingleEvent from "./Pages/AttendeesView/SingleEvent/SingleEvent";
+import Login from "./Pages/AttendeesView/Login/Login";
+import { HomePage } from "./Pages/AttendeesView/HomePage/HomePage";
+import { AllRoutes } from "./app/Routes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthenticatedApp from "./app/AuthenticatedApp";
+import UnauthenticatedApp from "./app/UnauthenticatedApp";
 function App() {
+  const user = true;
   return (
-    // <Event></Event>
-    // <Details />
-    // <Following />
-    // <Likes />
-    // <Following />
-    // <Signup />
-    // <>
-    <Login />
-    // {/* <Login></Login> */}
-    // </>
-    // <Layout>
-    //   <BrowseEventsPage />
-    // </Layout>
+    <>
+      {user ? (
+        <Layout>
+          <AuthenticatedApp />
+        </Layout>
+      ) : (
+        <UnauthenticatedApp />
+      )}
+    </>
   );
 }
 
