a1e5eeb468cd6c62f05ed26bd3f0bd21d46e59be
Yousef Gilany <yogilany@gmail.com>
2023-03-14 00:34:30 +0200

    updates

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,41 +1,38 @@
-import React from "react";
-import "./header.css";
-import Logo from "../../assets/logo.png";
-import { AiOutlineSearch } from "react-icons/ai";
-import { RiArrowDownSLine } from "react-icons/ri";
-import { CgProfile } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import React from 'react'
+import './header.css'
+import Logo from '../../assets/logo.png'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { RiArrowDownSLine } from 'react-icons/ri'
+import {CgProfile} from 'react-icons/cg'
 const Header = () => {
   return (
     <div className="header__container">
       <div className="header__container-left">
-        <a href="/">
-          <img src={Logo} alt="headerLogo" />
-        </a>
+        <img src={Logo} alt="headerLogo" />
         <input type="search" placeholder="Search event" />
       </div>
       <div className="header__container-links">
         <div className="header__container-link">
-          <a href="all-events">Create event</a>
+          <a href="#">Browse Events</a>
         </div>
         <div className="header__container-link">
-          <a href="/likes">Likes</a>
+          <a href="#">Create event</a>
         </div>
         <div className="header__container-link">
-          <a href="#"> Tickets </a>
+          <a href="#"> Help </a>
         </div>
         <div className="header__container-link">
           <a href="#"> Organize </a>
           <RiArrowDownSLine />
         </div>
         <div className="header__container-link">
-          <CgProfile className="profile" />
+          <CgProfile className='profile' />
           <a href="#">mahmoudkk177@gmail.com </a>
           <RiArrowDownSLine />
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Header;
+export default Header
