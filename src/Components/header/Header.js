ffb327679c1be40a6e097c0e5da1e95906c18aa3
Yousef Gilany <yogilany@gmail.com>
2023-03-14 00:45:21 +0200

    adding route

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -4,16 +4,19 @@ import Logo from "../../assets/logo.png";
 import { AiOutlineSearch } from "react-icons/ai";
 import { RiArrowDownSLine } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header__container">
       <div className="header__container-left">
-        <img src={Logo} alt="headerLogo" />
+        <a href="/">
+          <img src={Logo} alt="headerLogo" />
+        </a>
         <input type="search" placeholder="Search event" />
       </div>
       <div className="header__container-links">
         <div className="header__container-link">
-          <a href="#">Browse Events</a>
+          <a href="/likes">Likes</a>
         </div>
         <div className="header__container-link">
           <a href="#">Create event</a>
