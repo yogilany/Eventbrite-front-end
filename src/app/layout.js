e1ac865e05161db15d3235c7a9d4532ccc369fc9
Yousef Gilany <yogilany@gmail.com>
2023-03-14 00:27:08 +0200

    Added the new header

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,52 +9,17 @@ import {
 } from "react-bootstrap";
 import Loading from "../components/loading";
 import logo from "../assets/eventbrite-logo.svg";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 export const Layout = ({ children }) => {
   return (
     <>
-      <Navbar variant="light" className="navbarPrimary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              width="120"
-              //   height="30"
-              className="d-inline-block align-top"
-              alt="logo"
-            />
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-            </Form>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#home">Create an event</Nav.Link>
-            <Nav.Link href="#features">Likes</Nav.Link>
-            <Nav.Link href="#pricing">Tickets</Nav.Link>
-            <NavDropdown title="Yousef Gilany" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Header />
 
       <main id="centerView" className="">
         <Suspense fallback={<Loading />}>{children}</Suspense>
       </main>
+      <Footer />
     </>
   );
 };
