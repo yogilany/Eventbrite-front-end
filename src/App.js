0a5e66314738265e63af1061247084b6da095d85
Yousef Gilany <yogilany@gmail.com>
2023-03-26 21:02:09 +0200

    added server function to run the api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import "./App.css";
 import { Layout } from "./app/layout";
 import AuthenticatedApp from "./app/AuthenticatedApp";
 import UnauthenticatedApp from "./app/UnauthenticatedApp";
+import { useEffect } from "react";
+import server from "./backend/server";
 function App() {
   const user = true;
+
+  useEffect(() => {
+    server();
+  }, []);
+
   return (
     <>
       {user ? (
