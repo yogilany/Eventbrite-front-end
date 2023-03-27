f2aa273e8f08b9370f11a4d74ed5eedcfc924460
Yousef Gilany <yogilany@gmail.com>
2023-03-27 15:52:34 +0200

    connecting server

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import UnauthenticatedApp from "./app/UnauthenticatedApp";
 import { useEffect } from "react";
 import server from "./backend/server";
 function App() {
-<<<<<<< HEAD
-  const user = false;
-=======
   const user = true;
 
   useEffect(() => {
     server();
   }, []);
 
->>>>>>> a50bffd8b4420549dac717ee4400638bcb9ba1f1
   return (
     <>
       {user ? (
         //<Layout>
-         <Layout>
+        <Layout>
           <AuthenticatedApp />
-         </Layout>
+        </Layout>
       ) : (
         //</Layout>
         <UnauthenticatedApp />
