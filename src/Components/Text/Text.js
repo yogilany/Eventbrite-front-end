f8b12baa66de4db5a28afd3ca6e62e4e89444e33
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-03-27 19:59:19 +0200

    Modify Text Box

--- src/Components/Text/Text.js
+++ src/Components/Text/Text.js
@@ -2,11 +2,13 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import styled from "@emotion/styled";
-
+import { useRef , useState } from "react";
 export default function ValidationTextFields({data_testid}) {
     const MyText = styled(TextField)({
         width:'400px '
     });
+  const textRef = useRef();
+  const [textValue, setTextValue] = useState("");
   return (
     <Box
       component="form"
@@ -18,12 +20,15 @@ export default function ValidationTextFields({data_testid}) {
     >
       <div>
         <TextField
+          error={textValue === "" ? true : false}
           id="outlined-error"
           label="Summary"
           placeholder="Write a short event summary to get attended excited."
           style={{ width: "92%", marginTop: "5px" }}
           className="text"
-          data-testid = {data_testid}
+          data-testid={data_testid}
+          ref={textRef}
+          onChange = {(e) => {setTextValue(e.target.value)}}
         />
       </div>
     </Box>
