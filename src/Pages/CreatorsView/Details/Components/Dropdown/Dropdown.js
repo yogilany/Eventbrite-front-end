790202de8f525e49bc898b95bcedcaa243433681
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-03-28 21:34:23 +0200

    Add Generic Links in DropDown List

--- src/Pages/CreatorsView/Details/Components/Dropdown/Dropdown.js
+++ src/Pages/CreatorsView/Details/Components/Dropdown/Dropdown.js
@@ -2,17 +2,14 @@ import React from 'react'
 import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 import './Dropdown.css'
 import { useState } from 'react'
-const Dropdown = ({Name}) => {
-    const [toggleBtn, setToggleBtn] = useState(false);
+const Dropdown = ({Name , Links}) => {
+  const [toggleBtn, setToggleBtn] = useState(false);
   return (
     <div className = "Dropdown">
       <a data-testid = "dropdownLink" href="#" className='drop__btn' onClick={() => setToggleBtn(!toggleBtn)}>{Name} {toggleBtn ? <AiOutlineArrowUp className = "arrow__Logo"  /> : <AiOutlineArrowDown className = "arrow__Logo" />}</a>
       {toggleBtn &&
-        <div className='linkss' data-testid = "dropdownLinks">
-          <a href="#">Google</a>
-          <a href="#">Facebook</a>
-          <a href="#">Instagaram</a>
-          <a href="#">Whatsapp</a>
+        <div className='linkss' data-testid="dropdownLinks">
+          {Links.map((Link, i) => { return <a key={i}>{Link}</a>})}
         </div>}
     </div>
   )
