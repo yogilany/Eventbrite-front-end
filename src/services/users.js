772340b567efb1fd527f9dd408d598784670853b
Yousef Gilany <yogilany@gmail.com>
2023-03-28 14:37:02 +0200

    not needed anymore

--- src/services/users.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getUsers = ({ email, password }) => {
-  return axios.post("https://api.eventbrite.com/get-user", [], {
-    params: { email: email, password: password },
-  });
-};
