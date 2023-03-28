71c3c4c9fc2247e0460008924007cf9ed365a930
Yousef Gilany <yogilany@gmail.com>
2023-03-28 16:04:29 +0200

    Added new event covers

--- src/Pages/AttendeesView/HomePage/Components/EventCard.js
+++ src/Pages/AttendeesView/HomePage/Components/EventCard.js
@@ -6,6 +6,16 @@ import adelEv1 from "../../../../assets/adelEv1.png";
 import adelEv2 from "../../../../assets/adelEv2.png";
 import adelEv3 from "../../../../assets/adelEv3.png";
 import adelEv4 from "../../../../assets/adelEv4.png";
+import adelEv5 from "../../../../assets/adelEv5.png";
+import adelEv6 from "../../../../assets/adelEv6.png";
+import adelEv7 from "../../../../assets/adelEv7.png";
+import adelEv8 from "../../../../assets/adelEv8.png";
+import adelEv9 from "../../../../assets/adelEv9.png";
+import adelEv10 from "../../../../assets/adelEv10.png";
+import adelEv11 from "../../../../assets/adelEv11.png";
+import adelEv12 from "../../../../assets/adelEv12.png";
+import adelEv13 from "../../../../assets/adelEv13.png";
+import adelEv14 from "../../../../assets/adelEv14.png";
 
 import { format } from "date-fns";
 var parseISO = require("date-fns/parseISO");
@@ -28,7 +38,22 @@ const EventCard = ({
   eventOrganizer,
   organizerFollowers,
 }) => {
-  const eventsCover = [adelEv1, adelEv2, adelEv3, adelEv4];
+  const eventsCover = [
+    adelEv1,
+    adelEv2,
+    adelEv3,
+    adelEv4,
+    adelEv5,
+    adelEv6,
+    adelEv7,
+    adelEv8,
+    adelEv9,
+    adelEv10,
+    adelEv11,
+    adelEv12,
+    adelEv13,
+    adelEv14,
+  ];
 
   return (
     <Col xl={3} lg={4} md={4} sm={6} data-testid="event-card">
