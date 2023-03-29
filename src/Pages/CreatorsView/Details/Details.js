afd6524492194c123aeec6db0092cb3376663684
Yousef Gilany <yogilany@gmail.com>
2023-03-29 06:06:35 +0200

    fixes

--- src/Pages/CreatorsView/Details/Details.js
+++ src/Pages/CreatorsView/Details/Details.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
-import './Details.css'
-import EventImage from './Components/EventImage/EventImageBox'
-import Summary from './Components/SummaryBox/Summary'
-import Description from './Components/Description/Description'
-import AddEvents from './Components/AddEvents/AddEvents'
-import Sidebar from '../Sidebar/Sidebar'
-import Header from '../../../components/header/Header'
-import CreatorHeader from './Components/creatorHeader/CreatorHeader'
-import { createContext } from 'react'
+import React, { useState } from "react";
+import "./Details.css";
+import EventImage from "./Components/EventImage/EventImageBox";
+import Summary from "./Components/SummaryBox/Summary";
+import Description from "./Components/Description/Description";
+import AddEvents from "./Components/AddEvents/AddEvents";
+import Sidebar from "../Sidebar/Sidebar";
+import Header from "../../../components/header/Header";
+import CreatorHeader from "./Components/creatorHeader/CreatorHeader";
+import { createContext } from "react";
+import Headerpub from "../../Publishpage/Headerpub";
+
 export const AppContext = createContext({});
 /**
  * @author Mahmoud Khaled
@@ -18,31 +20,41 @@ export const AppContext = createContext({});
 const Details = () => {
   const handleForm = (e) => {
     e.preventDefault();
-  }
+  };
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [showSubmit, setShowSubmit] = useState(false);
-    return (
-      <AppContext.Provider value={{ toggleSidebar, setToggleSidebar , showSubmit ,setShowSubmit }}>
-        <div className="Details__page">
-          <CreatorHeader MenuShow={true} />
-          <Sidebar />
-          <form onSubmit={handleForm}>
-            <div className={`Details__container ${toggleSidebar ? "details__opacity" : ''}`} data-testid = "Details__contianer">
-              <EventImage />
-              <Summary />
-              <Description />
-              <AddEvents />
+  return (
+    <AppContext.Provider
+      value={{ toggleSidebar, setToggleSidebar, showSubmit, setShowSubmit }}
+    >
+      {/* <CreatorHeader MenuShow={true} /> */}
+      <Headerpub data_testid="HDID" />
+      <Sidebar />
+
+      <div className="Details__page">
+        <form onSubmit={handleForm}>
+          <div
+            className={`Details__container ${
+              toggleSidebar ? "details__opacity" : ""
+            }`}
+            data-testid="Details__contianer"
+          >
+            <EventImage />
+            <Summary />
+            <Description />
+            <AddEvents />
+          </div>
+          {showSubmit && (
+            <div className="submit__section" data-testid="submit__section">
+              <button className="discard__btn">Discard</button>
+              <button type="submit" className="submit__btn">
+                Save
+              </button>
             </div>
-            {showSubmit &&
-              <div className="submit__section" data-testid="submit__section">
-                <button className="discard__btn">Discard</button>
-                <button type="submit" className="submit__btn">
-                  Save
-                </button>
-              </div> }
-          </form>
-        </div>
-      </AppContext.Provider>
-    );
-  }
+          )}
+        </form>
+      </div>
+    </AppContext.Provider>
+  );
+};
 export default Details;
