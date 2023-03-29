342079b8132f510d14725ba1afa2d77af258cde7
Yousef Gilany <yogilany@gmail.com>
2023-03-29 03:52:10 +0200

    added service for posing user

--- src/services/services.js
+++ src/services/services.js
@@ -1,18 +1,53 @@
-export const getTodos = () => {
-  return fetch("http://localhost:8000/todos").then((response) =>
-    response.json()
-  );
+import axios from "axios";
+
+export const getUsers = async () => {
+  var usersData = [];
+
+  await axios
+    .get("http://localhost:8000/users")
+    // .then((response) => response.json())
+    .then((response) => {
+      console.log("response: ", response);
+      usersData = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return usersData;
 };
 
-export const getUsers = () => {
-  return fetch("http://localhost:8000/users").then((response) =>
-    response.json()
-  );
+export const addUser = async ({ name, username, email, password }) => {
+  await axios
+    .post("http://localhost:8000/users", {
+      name: { name },
+      username: { username },
+      email: { email },
+      password: { password },
+    })
+    // .then((response) => response.json())
+    .then((response) => {
+      console.log("response: ", response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return true;
 };
 
-export const getEvents = ({ category }) => {
-  console.log("categoryyy: ", category);
-  return fetch("http://localhost:8000/events").then((response) =>
-    response.json()
-  );
+export const getEvents = async ({ category }) => {
+  var eventsData = [];
+
+  await axios
+    .get("http://localhost:8000/events")
+    // .then((response) => response.json())
+    .then((response) => {
+      eventsData = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return eventsData;
 };
