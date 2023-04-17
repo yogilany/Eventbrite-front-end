a65147b3d3c1658c430bf37a260a44729495065e
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-04-17 15:48:17 +0200

    Fisxed Loading import

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,11 +1,7 @@
 import Spinner from "react-bootstrap/Spinner";
 
-function Loading() {
-  return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-  );
-}
-
-export default Loading;
+export const Loading = () => {
+  <Spinner animation="border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </Spinner>;
+};
