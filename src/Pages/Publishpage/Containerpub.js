daf508b8195e7966d421a28c2bfc666b02838934
ZiadEzzat133 <76906793+ZiadEzzat133@users.noreply.github.com>
2023-05-08 20:17:16 +0300

    Publish ewsponsive done

--- src/Pages/Publishpage/Containerpub.js
+++ src/Pages/Publishpage/Containerpub.js
@@ -15,11 +15,11 @@ const Containerpub = (props) => {
             <div >
                 <img src={eventphoto} className='Full__Container_img' />
             </div>
-            <div>
+            <div className='rtpart'>
                 <h1 style={{ marginTop: 28, marginLeft: 10, fontSize: 20 }}>Food Truck Festival</h1>
                 <div style={{ lineHeight: 0, marginTop: 12 }}>
-                    <p style={{ marginLeft: 10, fontSize: 12 }}>Tuesday, April 18, 2023 at 7:00 PM EET</p>
-                    <p style={{ marginLeft: 10, fontSize: 12 }}>Egypt, Cairo, Cairo Governorate 12212</p>
+                    <p style={{ marginLeft: 10, fontSize: 12,width:"150%" }}>Tuesday, April 18, 2023 at 7:00 PM EET</p>
+                    <p style={{ marginLeft: 10, fontSize: 12,width:"150%" }}>Egypt, Cairo, Cairo Governorate 12212</p>
                     <div style={{ marginTop: 25, marginRight: 10, display: 'flex' }}>
                         <TfiTicket style={{ width: 45, height: 20 }} />
                         <p style={{ marginTop: 10 }} >12</p>
@@ -31,9 +31,9 @@ const Containerpub = (props) => {
                     <div className='hline'>
                     </div>
                     <div style={{ display: 'block', marginLeft: '35%' }}>
-                        <div style={{ display: 'flex' }}>
-                            <p style={{ marginTop: 30, color: 'blue', fontSize: 14, cursor: 'pointer' }}>Preview your event</p>
-                            <FiExternalLink style={{ width: 45, height: 20, color: 'blue', textAlign: 'center', marginTop: 20, cursor: 'pointer' }} />
+                        <div className='prev' style={{ display: 'flex', width:"150%" }}>
+                            <p style={{ marginTop: 50, color: 'blue', fontSize: 14, cursor: 'pointer' }}>Preview your event</p>
+                            <FiExternalLink style={{ width: 45, height: 20, color: 'blue', textAlign: 'center', marginTop: 40, cursor: 'pointer' }} />
                         </div>
                     </div>
                 </div>
