b0164d3cb84c8fd352f605792d96656408a594d7
yogilany <yousef.gilany021@eng-st.cu.edu.eg>
2023-05-09 21:50:47 +0300

    styling

--- src/Pages/AttendeesView/SingleEvent/Components/AttendeeData.js
+++ src/Pages/AttendeesView/SingleEvent/Components/AttendeeData.js
@@ -1,15 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Form } from 'react-bootstrap'
 import TextInputStyled from "../../../../Components/TextInput/TextInput";
 import LoginMethodsCSS from "../../../AttendeesView/Login/Components/LoginMethods.module.scss";
 import { useForm } from 'react-hook-form'
 import { useState } from 'react';
+import axios from 'axios';
+import { is } from 'date-fns/locale';
 
-const AttendeeData = ({index, ticket}) => {
+const AttendeeData = ({index, ticket, sendData, isPlaceorder,event, token, orderID,setIsCheckoutDone, isCheckoutDone, setIsCheckout,total}) => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const [email, setEmail] = useState("");
-    const [fisrtName, setFisrtName] = useState("");
+    const [firstname, setFisrtName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [ attendee, setAttendee] = useState({})
+
+    const handleChangeAttendee = (e) => {
+        e.preventDefault();
+        if(e.target.name === "firstname"){
+            setFisrtName(e.target.value)  
+        }
+        if(e.target.name === "lastname"){
+            setLastName(e.target.value)
+        }
+        if(e.target.name === "email"){
+            setEmail(e.target.value)}
+
+        // detect the event key and target and add the value to the state 
+        setAttendee({...attendee, [e.target.name]: e.target.value})
+        
+        
+
+
+    }
+
+    const handleAddAttendee = async () => {
+
+
+      const Finalattendee = {
+        "first_name": firstname,
+        "last_name": lastName,
+        "email": email,
+        "type_of_reseved_ticket": ticket.ticket_info.name,
+        "order_id": orderID,
+        "event_id": event.id
+      }
+      console.log("Finalattendee",Finalattendee)
+
+
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BASE_API}/attendees/${event.id}/add_attendee`,
+          Finalattendee,
+          {
+            headers: { "Content-Type": "application/json" ,
+            Authorization: `Bearer ${token}`}
+          }
+        );
+        console.log("Res : ", response);
+        console.log("index -- total : ", index, total);
+        if(index === total ){
+          setIsCheckout(false)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+   
+useEffect( () => {
+
+  if(orderID){
+
+ 
+
+    handleAddAttendee();
+
+   
+  }
+
+}, [orderID])
+
+
+
+
+  
 
   return (
 
@@ -29,8 +104,8 @@ const AttendeeData = ({index, ticket}) => {
         autoComplete="off"
         name="firstname"
         required
-        value={fisrtName}
-        onChange={(e) => setFisrtName( e.target.value)}
+        value={firstname}
+        onChange={handleChangeAttendee}
         type="text"
         isInvalid={errors?.email}
       />
@@ -42,12 +117,11 @@ const AttendeeData = ({index, ticket}) => {
       <TextInputStyled
         id='login-email-input'
         data-testid='login-email-input'
-        {...register("email", { required: "Required" })}
         autoComplete="off"
         required
         name="lastname"
         value={lastName}
-        onChange={(e) => setLastName( e.target.value)}
+        onChange={handleChangeAttendee}
 
         type="text"
         isInvalid={errors?.email}
@@ -63,12 +137,11 @@ const AttendeeData = ({index, ticket}) => {
       <TextInputStyled
         id='login-email-input'
         data-testid='login-email-input'
-        {...register("email", { required: "Required" })}
         autoComplete="off"
         name="email"
         type="email"
         value={email}
-        onChange={(e) => setEmail( e.target.value)}
+        onChange={handleChangeAttendee}
         required
         isInvalid={errors?.email}
       />
