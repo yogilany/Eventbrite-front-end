76407898f1cf6ac1e0751cae3758ede5af577dcc
h4z3m <h4z3m.private@gmail.com>
2023-05-09 16:42:57 +0300

    -Fix formatting

--- src/app/ProtectedRoute.js
+++ src/app/ProtectedRoute.js
@@ -1,17 +1,17 @@
-import { Navigate, Outlet, Route, useLocation } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 // import { useAuth } from "./useAuth";
 import { selectUserToken } from "../features/authSlice";
 import { useSelector } from "react-redux";
 
 export const ProtectedRoute = () => {
-    const token = useSelector(selectUserToken);
-    // const { user } = useAuth();
-    const location = useLocation();
-    return token ? (
-        <Outlet />
-    ) : (
-        <Navigate to="/login" replace state={{ from: location }} />
-    );
+  const token = useSelector(selectUserToken);
+  // const { user } = useAuth();
+  const location = useLocation();
+  return token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
 };
 
 export default ProtectedRoute;
