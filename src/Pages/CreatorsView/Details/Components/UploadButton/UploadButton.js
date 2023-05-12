93204fba4318fe2555a301865df7b36ce1fbdea3
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-05-13 01:11:18 +0300

    Modify styles in Upload component in Details page

--- src/Pages/CreatorsView/Details/Components/UploadButton/UploadButton.js
+++ src/Pages/CreatorsView/Details/Components/UploadButton/UploadButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef } from "react";
 import { CgImage } from "react-icons/cg";
-import './uploadButton.css'
+import "./uploadButton.css";
 const UploadButton = () => {
   const hiddenFileInput = useRef(null);
   const handleClick = () => {
@@ -10,10 +10,10 @@ const UploadButton = () => {
   return (
     <>
       <button id="upload__btn" onClick={handleClick} className="upload__btn">
-        <div className='flex flex-row '> 
-        <CgImage className="img__logo" /><div className='ml-2'>        Upload Image
-</div>
-                  </div>
+        <div className="flex flex-row ">
+          <CgImage className="img__logo" />
+          <div className="ml-2"> Upload Image</div>
+        </div>
       </button>
       <input type="file" ref={hiddenFileInput} style={{ display: "none" }} />
     </>
