63c07f90eadb6a6107e9cfaece5d1e375bf22c31
ZiadEzzat133 <76906793+ZiadEzzat133@users.noreply.github.com>
2023-05-13 19:19:25 +0300

    publish updated

--- src/Pages/Publishpage/Containerpub.js
+++ src/Pages/Publishpage/Containerpub.js
@@ -13,15 +13,15 @@ const Containerpub = (props) => {
     const data = props.event;
     console.log("data ziad: ", data);
     const title = data.basic_info.title;
-    // const price = data?.tickets[0]?.price; // Events don't have tickets in usual case
-    // const quant = data?.tickets[0]?.max_quantity; // Events don't have tickets in usual case
+     const price = data?.tickets[0]?.price; // Events don't have tickets in usual case
+     const quant = data?.tickets[0]?.max_quantity; // Events don't have tickets in usual case
     const date =data.date_and_time.start_date_time;
     console.log("date",date);
     console.log("title");
     return (
         <div className='Full__Container' data-testid={props.data_testid}>
             <div >
-                <img src={data.image_link} className='Full__Container_img' />
+                <img src={data.image_link?data.image_link:eventphoto} className='Full__Container_img' />
             </div>
             <div className='rtpart'>
                 <h1 style={{ marginTop: 28, marginLeft: 10, fontSize: 20 }}>{title}</h1>
@@ -30,10 +30,10 @@ const Containerpub = (props) => {
                     <p style={{ marginLeft: 10, fontSize: 13, width: "150%",marginTop:5 }}>{data?.location?.city}</p>
                     <div style={{ marginTop: 18, marginRight: 10, display: 'flex' }}>
                         <TfiTicket style={{ width: 45, height: 20 }} />
-                        {/* <p style={{}} >{price}</p> // Events don't have tickets in usual case  */} 
+                        { <p style={{}} >{price}</p> }
                         <div style={{ marginLeft: 10, display: 'flex' }}>
                             <CgProfile style={{ width: 45, height: 20 }} />
-                            {/* <p style={{}} >{quant}</p>  // Events don't have tickets in usual case*/}
+                            <p style={{}} >{quant}</p> 
                         </div>
                     </div>
                     <div className='hline'>
